417f9b03a7a4a4e232fa752436bcaa918fd9752b
Jesus Castelan <jesuscastelan5@gmail.com>
2023-12-16 19:21:41 -0600

    indented all functions

--- graph.cpp
+++ graph.cpp
@@ -21,12 +21,12 @@ typedef struct{
 		thisName - name of node
 */
 void createNode(node * pThisNode, std::string thisName){
-*(pThisNode).ID = extern int globalID;
-extern std::vector<node> nodeDirectory;
-*(pThisNode).name = thisName;
-nodeDirectory.push_back (*(pThisNode));
-globalID++;
-return;
+	*(pThisNode).ID = extern int globalID;
+	extern std::vector<node> nodeDirectory;
+	*(pThisNode).name = thisName;
+	nodeDirectory.push_back (*(pThisNode));
+	globalID++;
+	return;
 }
 
 /*
@@ -35,9 +35,9 @@ return;
 	Param:
 */
 void insertChild (node * parentNode, node * childNode){
-int childID = *(childNode).ID;
-*(parentNode).childList.insert(childID);
-return;
+	int childID = *(childNode).ID;
+	*(parentNode).childList.insert(childID);
+	return;
 }
 
 /*
@@ -46,14 +46,14 @@ return;
 	Param:
 */
 void mergeBranches(node * childNode, int parent1ID, int parent2ID){
-extern std::vector<node> nodeDirectory;
+	extern std::vector<node> nodeDirectory;
 
-node parent1 = nodeDirectory[parent1ID]
-node parent2 = nodeDirectory[parent2ID]
+	node parent1 = nodeDirectory[parent1ID]
+	node parent2 = nodeDirectory[parent2ID]
 
-insertChild (&parent1, &childNode)
-insertChild (&parent2, &childNode)
-return 0;
+	insertChild (&parent1, &childNode)
+	insertChild (&parent2, &childNode)
+	return;
 }
 
 
@@ -64,20 +64,21 @@ int globalID = 0;
 	Description: gives users a list of possible matches to parentName
 */
 std::vector<int> getParentID (std::string parentName){
-extern std::vector<node> nodeDirectory;
-std::vector <int> possMatches;
-for (auto thisNode : nodeDirectory){
-	if (thisNode.name == parentName){
-		possMatches.push_back(thisNode.ID);
+	extern std::vector<node> nodeDirectory;
+	std::vector <int> possMatches;
+	for (auto thisNode : nodeDirectory){
+		if (thisNode.name == parentName){
+			possMatches.push_back(thisNode.ID);
+		}
 	}
-}
-return possMatches;
+	return possMatches;
 }
 
 void promptName(std::string prompt, node * newNode){
-std::cout << prompt << std::endl;
-std::cin >> nodeName;
-createNode (newNode, nodeName);
+	std::cout << prompt << std::endl;
+	std::cin >> nodeName;
+	createNode (newNode, nodeName);
+	return;
 }
 
 /*
@@ -86,29 +87,29 @@ createNode (newNode, nodeName);
 	Param:
 */
 void addNodes(){
-extern int globalID;
-extern std::vector<node> nodeDirectory;
+	extern int globalID;
+	extern std::vector<node> nodeDirectory;
 
-char userAns;
-std::cout << "Does this family member have a parent node (y/n)? " << std::endl;
-std::cin >> userAns;
-std::string nodeName;
-if (userAns == 'n'){
+	char userAns;
+	std::cout << "Does this family member have a parent node (y/n)? " << std::endl;
+	std::cin >> userAns;
+	std::string nodeName;
+	if (userAns == 'n'){
+		node newNode;
+		promptName ("What is the name of the family member / marriage? ", &newNode);
+		return;
+	}
+	std::string parentName, nodeName;
+	std::cout << "What is the name of the parent / parent marriage? " << std::endl;
+	std::cin >> parentName;
+	// at some point, find a way to disambiguate apparent duplicate entries
+	int parentID = getParentID (parentName)[0];
+
 	node newNode;
-	promptName ("What is the name of the family member / marriage? ", &newNode);
+	promptName("What is the name of the new family member / marriage? ", &newNode)
+	node parentNode = nodeDirectory[parentID];
+	insertChild(&parentNode, &newNode);
 	return;
-}
-std::string parentName, nodeName;
-std::cout << "What is the name of the parent / parent marriage? " << std::endl;
-std::cin >> parentName;
-// at some point, find a way to disambiguate apparent duplicate entries
-int parentID = getParentID (parentName)[0];
-
-node newNode;
-promptName("What is the name of the new family member / marriage? ", &newNode)
-node parentNode = nodeDirectory[parentID];
-insertChild(&parentNode, &newNode);
-return;
 }
 
 
