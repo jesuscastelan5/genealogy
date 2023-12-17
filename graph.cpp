d9e3b0d6bccd0643aff966f61bfeceb6fa703874
Jesus Castelan <jesuscastelan5@gmail.com>
2023-12-16 21:15:59 -0600

    to_string isn't working, leading to csv not getting generated

--- graph.cpp
+++ graph.cpp
@@ -23,9 +23,9 @@ typedef struct{
 void createNode(node * pThisNode, std::string thisName){
 	extern int globalID;
 	pThisNode->ID = globalID;
-	extern std::vector<node> nodeDirectory;
+	extern std::vector<node *> nodeDirectory;
 	pThisNode->name = thisName;
-	nodeDirectory.push_back (*(pThisNode));
+	nodeDirectory.push_back (pThisNode);
 	globalID++;
 	return;
 }
@@ -62,29 +62,29 @@ void insertChild (node * parentNode, node * childNode){
 	Param:
 */
 void mergeBranches(node * childNode, int parent1ID, int parent2ID){
-	extern std::vector<node> nodeDirectory;
+	extern std::vector<node *> nodeDirectory;
 
-	node parent1 = nodeDirectory[parent1ID];
-	node parent2 = nodeDirectory[parent2ID];
+	node * pParent1 = nodeDirectory[parent1ID];
+	node * pParent2 = nodeDirectory[parent2ID];
 
-	insertChild (&parent1, childNode);
-	insertChild (&parent2, childNode);
+	insertChild (pParent1, childNode);
+	insertChild (pParent2, childNode);
 	return;
 }
 
 
-std::vector<node> nodeDirectory;
+std::vector<node *> nodeDirectory;
 int globalID = 0;
 
 /*
 	Description: gives users a list of possible matches to parentName
 */
 std::vector<int> getParentID (std::string parentName){
-	extern std::vector<node> nodeDirectory;
+	extern std::vector<node *> nodeDirectory;
 	std::vector <int> possMatches;
-	for (auto thisNode : nodeDirectory){
-		if (thisNode.name == parentName){
-			possMatches.push_back(thisNode.ID);
+	for (auto pThisNode : nodeDirectory){
+		if (pThisNode->name == parentName){
+			possMatches.push_back(pThisNode->ID);
 		}
 	}
 	return possMatches;
@@ -105,7 +105,7 @@ void promptName(std::string prompt, node * newNode){
 */
 void addNodes(){
 	extern int globalID;
-	extern std::vector<node> nodeDirectory;
+	extern std::vector<node *> nodeDirectory;
 
 	char userAns;
 	std::cout << "Does this family member have a parent node (y/n)? " << std::endl;
@@ -124,8 +124,8 @@ void addNodes(){
 
 	node newNode;
 	promptName("What is the name of the new family member / marriage? ", &newNode);
-	node parentNode = nodeDirectory[parentID];
-	insertChild(&parentNode, &newNode);
+	node * pParentNode = nodeDirectory[parentID];
+	insertChild(pParentNode, &newNode);
 	return;
 }
 
@@ -133,7 +133,7 @@ void addNodes(){
 std::string childToList (node * pParent){
 	std::string listOChildren;
 	int i;
-	for (i = 0; i < pParent->childList.size() - 1; i++){
+	for (i = 0; i < pParent->childList.size(); i++){
 		int child = pParent->childList[i];
 		listOChildren = listOChildren + std::to_string(child);
 		listOChildren.push_back(',');
@@ -145,13 +145,13 @@ std::string childToList (node * pParent){
 }
 
 void saveGeneology(std::string fileName){
-	extern std::vector<node> nodeDirectory;
+	extern std::vector<node *> nodeDirectory;
 	
 	std::ofstream myFile;
 	myFile.open(fileName + ".csv");
 	for (auto entry : nodeDirectory){
-		std::string childList = childToList (&entry);
-		myFile << std::to_string(entry.ID) << ";" << entry.name << ";" << childList << std::endl;
+		//std::string childList = childToList (entry);
+		myFile << std::to_string(entry->ID) << ";" << entry->name << std::endl;
 	}
 
 	myFile.close();
